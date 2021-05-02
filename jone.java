0c9abedca989a7917347d179c58293416b745ce8
sanika <sanikaraul06@gmail.com>
2021-05-02 12:05:10 +0530

    First Commit

---
+++ jone.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jone {
+
+    public static void main(String[] args) {
+
+        
+     
+        System.out.println("You entered into job1 " );
+    }
+}
